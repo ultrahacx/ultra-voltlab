462d6639e6bf53b9ce240945e2592db962cfb295
E0HH <44152393+E0HH@users.noreply.github.com>
2021-07-16 18:44:14 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,7 +9,19 @@ files {
     'html/*.html',
     'html/*.gif',
     'html/*.png',
+    'dlc_hei4/dlc_hei4_v_mg.awc',
+    'dlc_hei4/dlc_hei4_fh_mg.awc',
+    'audiodata/dlchei4_game.dat151',
+    'audiodata/dlchei4_game.dat151.nametable',
+    'audiodata/dlchei4_game.dat151.rel',
+    'audiodata/dlchei4_sounds.dat54',
+    'audiodata/dlchei4_sounds.dat54.nametable',
+    'audiodata/dlchei4_sounds.dat54.rel',
 }
+
+data_file 'AUDIO_GAMEDATA' 'audiodata/dlchei4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audiodata/dlchei4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'dlc_hei4'
 
 ui_page 'html/screen.html'
 
